8879f4ded416a151fc45ea5302afdb5dbd1be5ed
feraskhemakhem <feraskhemakhem@gmail.com>
2021-06-17 02:21:00 +0000

    setting elo and extracting ids complete

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 // IDEAs:  - add functionality to read names from a discord voice chat instead of waiting for reactions
 //         - add option to move people to given channels (automatically move to Val and Val2)
-
+//         - make setting elo reaction-based
 
 // includes
 
@@ -21,6 +21,8 @@ const random_dict = {
     'max' : 15,
     'andrew' : 6,
     'tammi' : 7,
+    '364583155623264256' : 9,
+    '721167637006123088' : 3,
 };
 
  
@@ -33,17 +35,20 @@ client.on('message', async message => {
         var num_players = parseInt(message.content.match(digits));
 
         // small error checking for number of players
-        if (num_players < 0 || isNaN(num_players)) {
+        if (isNaN(num_players)) {
+            await message.channel.send('Please follow the format: \"!match <number of players>\"');
+        }
+        else if (num_players < 0) {
             await message.channel.send('Only non-negative numbers allowed');
             return;
         }
-        console.log(num_players);
+
 
         let ids = [];
 
         // using await (https://discordjs.guide/additional-info/async-await.html#execution-with-discord-js-code)
         try {
-            const reply = await message.channel.send('x.Please react :white_check_mark: if you wish to participate in the game');
+            const reply = await message.channel.send('Please react :white_check_mark: if you wish to participate in the game');
             await reply.react('✅');
 
             const filter = (reaction, user) => {
@@ -51,28 +56,47 @@ client.on('message', async message => {
             };
 
             // structure inspired by https://stackoverflow.com/questions/50058056/how-to-use-awaitreactions-in-guildmemberadd
-            reply.awaitReactions(filter, { max: num_players, time: 10000, errors: ['time'] }) // waiting 1 minute for 1 responses
+            reply.awaitReactions(filter, { max: num_players, time: 60000, errors: ['time'] }) // waiting 1 minute for 1 responses
                 .then(collected => {
-                    message.channel.send('x.Responses recorded...');
+                    message.channel.send('Responses recorded...');
 
                     // extract IDs of reactors
-                    ids = Array.from(collected.values());
-                    ids = Array.from(ids[0].users.keys());
+                    // EACH COLLECTED CONSISTS OF THE FOLLOWING:
+                    // KEY IS THE EMOJI
+                    // VALUE IS THE FOLLOWING: MESSAGEID (ID STRING), USERS (ID STRINGS), COUNT (# REACTIONS), ME (BOOL)
+                    // https://discord.js.org/#/docs/main/stable/class/MessageReaction
+                    // https://discord.js.org/#/docs/main/stable/class/ReactionUserManager
+                    let temp_count = collected.first().count;
+                    ids = Array.from(collected.first().users.cache.firstKey(temp_count)); // apparently i need all of this for ids
 
-                    message.channel.send('x.Polling is complete. Making teams...');
+                    // if bot's ID exists in list, remove
+                    let index_of_my_id;
+                    if ((index_of_my_id = ids.indexOf(client.user.id)) !== -1) {
+                        ids.splice(index_of_my_id, 1);
+                    }
+
+                    message.channel.send('Polling is complete. Making teams...');
+
+
+                    // find these ids in the list and make a dictionary of their elos
+                    let elos = {};
+                    ids.forEach(element => {
+                        if (random_dict[element]) {
+                            elos[element] = random_dict[element];
+                        }
+                    });
+
+                    message.channel.send(`elos are: ${JSON.stringify(elos)}`);
+
 
                     // make the teams
-                    // findTeams(ids, random_dict);
-
-                    // print ids at discord users
-                    // message.channel.send('x.Team1:');
 
                 })
                 .catch(collected => { 
-            	console.log(`x.After a minute, only ${collected.size} out of ${num_players} reacted.`);
+            	console.log(`Collected is ${JSON.stringify(collected)}. After a minute, only ${collected.size} out of ${num_players} reacted.`);
             });
         } catch (error) {
-            console.log('x.error replying and reacting');
+            console.log('error replying and reacting');
         }
 
 
@@ -92,11 +116,19 @@ client.on('message', async message => {
         var elo_bracket = elo.substring(0, elo.indexOf(' '));
         var elo_number = parseInt(elo.substring(elo.indexOf(' ') + 1)) || -1;
 
-
-        console.log(elo, elo_bracket, elo_number);
+        // edge case for no subrank number
+        if (elo === 'radiant') {
+            elo_bracket = 'radiant';
+            elo_number = 0;
+        }
         // small error checking
-        if (elo_number < 0 || elo_number > 3) {
-            await message.channel.send('Only valid inputs are allowed');
+        else if (elo === -1) {
+            await message.channel.send('Please follow the format: \"!setelo <rank bracket> <subrank number>');
+            return;
+        }
+        // in case number is out of range
+        else if (elo_number < 0 || elo_number > 3) {
+            await message.channel.send('Please enter a valid subrank number');
             return;
         }
 
@@ -142,9 +174,10 @@ client.on('message', async message => {
         score += elo_number;
 
         // TODO: add entry with user key and score to server
+        random_dict[message.author.id] = score;
 
         // send message to confirm score value
-        await message.channel.send('your score is ' + score);
+        await message.channel.send(`your rank was registered as ${elo}`);
     }
 
     else if (message.content.startsWith('!reroll')) { // in case we don't like the teams, we can reroll
