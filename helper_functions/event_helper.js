4ab14f0af6219a5493bb40e9517b924b48b951d6
feraskhemakhem <feraskhemakhem@gmail.com>
2021-07-09 02:37:31 +0100

    made it possible to get response message after sending it

--- helper_functions/event_helper.js
+++ helper_functions/event_helper.js
@@ -11,12 +11,25 @@ module.exports = {
                 data = await createAPIMessage(interaction, response);
         }
 
+        // POST V13 CODE
+        // await interaction.reply(response);
+        // return await interaction.fetchReply();
+
         //  reply? TODO: since discord.js doesnt natively support it yet, doing it ugly
-        client.api.interactions(interaction.id, interaction.token).callback.post({
+        await client.api.interactions(interaction.id, interaction.token).callback.post({
                 data: {
                         type: 4, // CHANNEL_MESSAGE_WITH_SOURCE
                         data: data,
                 }
         });
+
+        const app = client.fetchApplication();
+        const reply = await client.api.webhooks(client.user.id, interaction.token).messages('@original').get();
+
+        console.log(`reply sent for response: ${response} and ${typeof{reply}}, ${JSON.stringify(reply)}`);
+        return reply;
+    },
+    followUp: async function(client, interaction, response) {
+        new Discord.WebhookClient(client.user.id, interaction.token).send(response);
     }
 };
